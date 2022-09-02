cff1a32a8cafc67ec44cbe77736588c929518f1c
Alhassan Matarwy <67296782+Matarwy@users.noreply.github.com>
2022-09-02 16:12:12 +0200

    init

---
+++ Process.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Project
+{
+    internal class Process
+    {
+    }
+}
